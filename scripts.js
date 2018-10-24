069b6f2dce4a50ca5382f3dfae26ba8d6f259759
Diego Versli <diego.versali@stealthelook.net>
2018-10-24 14:30:30 -0300

    Foi feito uma alteração no JS.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
-console.log("test");
+var btn1 = document.getElementById("btn-1");
+var btn2 = document.getElementById("btn-2");
+var btn3 = document.getElementById("btn-3");
+
+btn1.addEventListener('click', function(){ 
+    
+	alert("botao 1");
+
+}, false);
+
+btn2.addEventListener('click', function(){ 
+    
+	alert("botao 2");
+
+}, false);
+
+btn3.addEventListener('click', function(){ 
+    
+	alert("botao 3");
+
+}, false);
+
+
+// document.addEventListener('keydown', function(){ 
+    
+// 	alert("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure eveniet cumque consequuntur perspiciatis sunt, illo nobis odit, inventore necessitatibus! Pariatur nesciunt officia veritatis eos quibusdam mollitia cupiditate quo qui.")
+
+// }, false);
+

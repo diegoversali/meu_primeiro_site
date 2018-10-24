0f268d53142bea26141d60f49191d0a1d797763f
Diego Versli <diego.versali@stealthelook.net>
2018-10-24 17:04:31 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,29 +1,33 @@
-var btn1 = document.getElementById("btn-1");
-var btn2 = document.getElementById("btn-2");
-var btn3 = document.getElementById("btn-3");
+// var btn1 = document.getElementById("btn-1");
+// var btn2 = document.getElementById("btn-2");
+// var btn3 = document.getElementById("btn-3");
 
-btn1.addEventListener('click', function(){ 
+// btn1.addEventListener('click', function(){ 
     
-	alert("botao 1");
-
-}, false);
-
-btn2.addEventListener('click', function(){ 
-    
-	alert("botao 2");
-
-}, false);
-
-btn3.addEventListener('click', function(){ 
-    
-	alert("botao 3");
-
-}, false);
-
-
-// document.addEventListener('keydown', function(){ 
-    
-// 	alert("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure eveniet cumque consequuntur perspiciatis sunt, illo nobis odit, inventore necessitatibus! Pariatur nesciunt officia veritatis eos quibusdam mollitia cupiditate quo qui.")
+// 	alert("botao 1");
 
 // }, false);
 
+// btn2.addEventListener('click', function(){ 
+    
+// 	alert("botao 2");
+
+// }, false);
+
+// btn3.addEventListener('click', function(){ 
+    
+// 	alert("botao 3");
+
+// }, false);
+
+// 0var users = fetch('https://randomuser.me/api/');
+
+var r, usuario, email, telefone;    
+
+fetch('https://randomuser.me/api/')
+.then( (resp) => resp.json() )
+.then( function(data) {
+    r = data.results[0];
+    usuario = r.login.username;
+    console.log(r, usuario);
+});
